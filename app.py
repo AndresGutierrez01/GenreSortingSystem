d8717459ace54850499d5aff8df09382e3a37882
Andres Gutierrez <andygtz675@gmail.com>
2018-11-23 15:35:19 -0600

    wrote more functions in parseBook and added some books to BookFiles/

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from parsebook import Book
-from bookNBC import bookNBC
+import numpy as np
+# from bookNBC import bookNBC
 
 
-b = Book("Anna Karenin.txt")
+AK = Book("Anna-Karenin.txt")
+TSAF = Book("'Tween-Snow-and-Fire.txt")
+EMMA = Book("Emma.txt")
+HHH = Book("Healing-Her-Heart.txt")
+TROTN = Book("The-Return-of-The-Native.txt")
+
+# books = np.array([AK,TSAF,EMMA,HHH,TROTN])
+books = [AK, TSAF, EMMA, HHH, TROTN]
+words = ["love","adore","kiss","thing"]
 
 
-values = b.get_count("ice")
+values = Book.get_counts(books, words)
+
 
 print(values)
+
+
+print(Book.get_ratios(books, words))
+
+
+
+# print(AK.get_count(words))
+# print(AK.get_len())
+
+
