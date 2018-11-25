620e5015e4ea40df545e49f7786a802ee9a248d0
Andres Gutierrez <andygtz675@gmail.com>
2018-11-24 18:25:42 -0600

    fixed comment

--- testBookNBC.py
+++ testBookNBC.py
@@ -4,7 +4,7 @@ from bookNBC import bookNBC
 
 
 
-train = np.loadtxt('iris.csv', delimiter=',', dtype = np.float32) # first column is class label
+train = np.loadtxt('iris.csv', delimiter=',', dtype = np.float32) # last column is class label
 
 np.random.shuffle(train)
 
